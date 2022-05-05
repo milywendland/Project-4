57786d494d313f99ded8fcab9ae8110030a1378e
milywendland <milynicwendland@gmail.com>
2022-05-05 13:43:46 -0600

    started gift seed

---
+++ seed/gifts.js
@@ -0,0 +1,211 @@
+const { type } = require('os')
+const db = require('../db')
+const { Gift } = require('../models')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+const main = async () => {
+  const gifts = [
+    {
+      name: 'The Communist Manifesto',
+      type: 'Book',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'MAP OF THE SOUL: PERSONA by BTS',
+      type: 'Album',
+      buff: 'Mood'
+    },
+    {
+      name: 'Nintendo Switch',
+      type: 'Electronics',
+      buff: 'Mood'
+    },
+    {
+      name: 'Box Fan',
+      type: 'Appliance',
+      buff: 'Mood'
+    },
+    {
+      name: 'Pride Flag',
+      type: 'Misc',
+      buff: 'Personality'
+    },
+    {
+      name: 'Gold Bar',
+      type: 'Treasure',
+      buff: 'Mood'
+    },
+    {
+      name: 'Baby Grand Piano',
+      type: 'Instrument',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'T-Mobile Sidekick',
+      type: 'Electronics',
+      buff: 'Mood'
+    },
+    {
+      name: 'Tamagotchi',
+      type: 'Electronics',
+      buff: 'Mood'
+    },
+    {
+      name: 'Mochi Ice Cream - Melted in the Microwave',
+      type: 'Snack',
+      buff: 'Mood'
+    },
+    {
+      name: 'BTS Light Stick',
+      type: 'Treasure',
+      buff: 'Personality'
+    },
+    {
+      name: 'Pokemon Card: Magikarp',
+      type: 'Treasure',
+      buff: 'Mood'
+    },
+    {
+      name: 'Train Set',
+      type: 'Toy',
+      buff: 'Mood'
+    },
+    {
+      name: 'A Single Tube Sock',
+      type: 'Treasure',
+      buff: 'Mood'
+    },
+    {
+      name: 'Sewing Machine',
+      type: 'Appliance',
+      buff: 'Mood'
+    },
+    {
+      name: 'Elder Wand',
+      type: 'Arcane',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'Super 8',
+      type: 'Electronics',
+      buff: 'Personality'
+    },
+    {
+      name: 'Bucket Hat',
+      type: 'Misc',
+      buff: 'Personality'
+    },
+    {
+      name: 'Water Color Set',
+      type: 'Misc',
+      buff: 'Mood'
+    },
+    {
+      name: 'Worlds Largest Rubber Band Ball',
+      type: 'Treasure',
+      buff: 'Mood'
+    },
+    {
+      name: 'Dinosaur Bone',
+      type: 'Treasure',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'Vespa',
+      type: 'Misc',
+      buff: 'Personality'
+    },
+    {
+      name: 'Toy Robot',
+      type: 'Toy',
+      buff: 'Mood'
+    },
+    {
+      name: 'Ship in a Bottle',
+      type: 'Misc',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'Polaroid Camera',
+      type: 'Electronics',
+      buff: 'Personality'
+    },
+    {
+      name: 'Set of Chopsticks',
+      type: 'Treasure',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'Shiny Rock',
+      type: 'Treasure',
+      buff: 'Mood'
+    },
+    {
+      name: 'Dyson Vacuum',
+      type: 'Appliance',
+      buff: 'Mood'
+    },
+    {
+      name: 'Pair of Crocs',
+      type: 'Treasure',
+      buff: 'Personality'
+    },
+    {
+      name: 'Chainsaw',
+      type: 'Misc',
+      buff: 'Mood'
+    },
+    {
+      name: 'Bagel',
+      type: 'Snack',
+      buff: 'Mood'
+    },
+    {
+      name: 'Pokemon Ball',
+      type: 'Toy',
+      buff: 'Mood'
+    },
+    {
+      name: 'A Quarter',
+      type: 'Treasure',
+      buff: 'Knowledge'
+    },
+    {
+      name: 'Hatsune Miku Figure',
+      type: 'Toy',
+      buff: 'Mood'
+    },
+    {
+      name: 'Barbie Dreamhouse',
+      type: 'Toy',
+      buff: 'Mood'
+    },
+    {
+      name: 'Sony TC-1210 Walkman',
+      type: 'Electronics',
+      buff: 'Mood'
+    },
+    {
+      name: 'Electric Toothbrush',
+      type: 'Misc',
+      buff: 'Mood'
+    },
+    {
+      name: 'Incense Stick',
+      type: 'Misc',
+      buff: 'Mood'
+    }
+  ]
+
+  await Gift.insertMany(gifts)
+
+  console.log('Created Gifts!')
+}
+
+const run = async () => {
+  await db.dropDatabase()
+  await main()
+  db.close()
+}
+
+run()
--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const db = require('./db')
 const cors = require('cors')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
+const { auth } = require('express-openid-connect')
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'CX5fWLn9xXb8UjLU3vqHfj2BkKDhV4Lb',
+  issuerBaseURL: 'https://dev-0bil0zze.us.auth0.com'
+}
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+app.use(auth(config))
 
 app.use('/api', routes)
 
@@ -17,4 +28,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 })
