ad23a7a031fe13035287ab93ee0bfc1e2b28f8ec
milywendland <milynicwendland@gmail.com>
2022-05-06 13:02:37 -0600

    undid game function

--- client/src/pages/gameplay.js
+++ client/src/pages/gameplay.js
@@ -1,62 +1,6 @@
 import WALKMANFIN from '../images/WALKMANFIN.png'
 
 const Gameplay = () => {
-  let foodAmount = 10
-  let playAmount = 10
-  let sleepAmount = 10
-  let happinessAmount = 10
-
-  let game = {
-    timePasses: function () {
-      foodAmount = foodAmount - 1
-      playAmount = playAmount - 2
-      sleepAmount = sleepAmount - 1
-      happinessAmount = happinessAmount - 1
-    },
-
-    isAlive: function () {
-      if (foodAmount <= 0) {
-        return false
-      } else {
-        return true
-      }
-    },
-
-    isSad: function () {
-      if (playAmount <= 3 || sleepAmount <= 3 || happinessAmount <= 4) {
-        return true
-      } else {
-        return false
-      }
-    },
-
-    isSick: function () {
-      if (foodAmount <= 3) {
-        return true
-      } else {
-        return false
-      }
-    },
-
-    foodAdd: function () {
-      foodAmount = foodAmount + 1
-    },
-
-    playAdd: function () {
-      playAmount = playAmount + 1
-    },
-
-    sleepAdd: function () {
-      sleepAmount = sleepAmount + 1
-    },
-
-    happinessAdd: function () {
-      happinessAmount = happinessAmount + 1
-    },
-
-    didVisit: function () {}
-  }
-
   return (
     <div>
       <div className="console">
