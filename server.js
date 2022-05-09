adfb9d5600ce1116bb884d880705ad2ad7501f20
milywendland <milynicwendland@gmail.com>
2022-05-09 12:10:18 -0600

    auth router

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ const logger = require('morgan')
 const PORT = process.env.PORT || 3001
 const { auth } = require('express-openid-connect')
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'CX5fWLn9xXb8UjLU3vqHfj2BkKDhV4Lb',
-  issuerBaseURL: 'https://dev-0bil0zze.us.auth0.com'
-}
-
 const app = express()
 
 app.use(cors())
