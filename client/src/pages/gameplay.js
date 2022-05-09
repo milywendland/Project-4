87c57c92895edeec389265da7f714c5688a6aeaf
milywendland <milynicwendland@gmail.com>
2022-05-09 12:57:24 -0600

    console buttons setup

--- client/src/pages/gameplay.js
+++ client/src/pages/gameplay.js
@@ -1,5 +1,10 @@
 import WALKMANFIN from '../images/WALKMANFIN.png'
+import room from '../images/room.png'
 import { useNavigate } from 'react-router-dom'
+import MILKAPPA from '../images/MILKAPPA.png'
+import MILKITSUNE from '../images/MILKITSUNE.png'
+import MILTTANUKI from '../images/MILTTANUKI.png'
+import MILSCANTI from '../images/MILSCANTI.png'
 
 const Gameplay = () => {
   const navigate = useNavigate()
@@ -9,6 +14,12 @@ const Gameplay = () => {
       <div className="console">
         <img src={WALKMANFIN} alt="console" />
       </div>
+      <div className="console-pet">
+        <img src={MILSCANTI} alt="canti" />
+      </div>
+      <div className="left-button"></div>
+      <div className="right-button"></div>
+      <div className="surprise-button"></div>
       <button onClick={() => navigate('/choosepet')}>Choose A Pet</button>
     </div>
   )
