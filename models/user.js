4b4d03719e2358280b9d0afd1cdd38e1fb52e4da
milywendland <milynicwendland@gmail.com>
2022-05-10 11:53:58 -0600

    user auth issues

--- models/user.js
+++ models/user.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcyrpt')
 const Schema = mongoose.Schema
 
 const User = new Schema(
   {
     username: { type: String, required: true },
-    password: { type: String, required: true },
-    token: { type: String, required: true },
+    hash_password: { type: String, required: true },
+    created: { type: Date, default: Date.now },
     pets: [{ type: Schema.Types.ObjectId, ref: 'pets' }]
   },
   { timestamps: true }
 )
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.hash_password)
+}
 
 module.exports = User
