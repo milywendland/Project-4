131b8a321379eaf7e0bc1a3d2bb15768388203d9
milywendland <milynicwendland@gmail.com>
2022-05-11 21:35:56 -0600

    added to profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -2,6 +2,7 @@ const Profile = () => {
   return (
     <div>
       <h2>Hey!</h2>
+      <img src="" alt="your pet" />
       <div className="pet-feed"></div>
       <button>Delete Pet :[</button>
     </div>
