e825d9f4d9dd70d19c98e53c425e9d31190d580d
milywendland <milynicwendland@gmail.com>
2022-05-11 21:38:37 -0600

    home page edits

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -3,7 +3,9 @@ const Profile = () => {
     <div>
       <h2>Hey!</h2>
       <img src="" alt="your pet" />
-      <div className="pet-feed"></div>
+      <div className="pet-feed">
+        <p>This is where the feed of activity will live</p>
+      </div>
       <button>Delete Pet :[</button>
     </div>
   )
