350907c9b0e1937dee0fd5991aff0aa9c057ecb4
milywendland <milynicwendland@gmail.com>
2022-05-16 07:51:49 -0600

    fixing db index

--- db/index.js
+++ db/index.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 const option = {
   socketTimeoutMS: 30000,
   keepAlive: true,
   reconnectTries: 30000
 }
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/ikimonoDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/ikimonoDatabase'
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
